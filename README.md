4894394fae0f19676094e8ce97433e9473a11864
Douglas Wong <diwong@edu.uwaterloo.ca>
2019-03-06 13:09:09 -0500

    very valuable commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# leetcode-practice
-
-Solutions to various problems on LeetCode, done in Python. Feel free to reference.
--- maximum-subarray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        for i in range(1, len(nums)):
-            nums[i] = max(nums[i-1] + nums[i], nums[i])
-        return max(nums)
